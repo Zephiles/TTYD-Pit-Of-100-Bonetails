831ad1ed87d3a3b9eb76f066920c18704c0d7022
Zephiles <cae222@yahoo.com>
2018-06-22 05:49:53 -0500

    Fix indentations

--- ttyd-tools/rel/source/mod.cpp
+++ ttyd-tools/rel/source/mod.cpp
@@ -14,23 +14,23 @@ Mod *gMod = nullptr;
 
 void main()
 {
-	Mod *mod = new Mod();
-	mod->init();
+  Mod *mod = new Mod();
+  mod->init();
 }
 
 Mod::Mod()
 {
-	
+  
 }
 
 void Mod::init()
 {
-	gMod = this;
-	
-	mPFN_makeKey_trampoline = patch::hookFunction(ttyd::system::makeKey, []()
-	{
-		gMod->updateEarly();
-	});
+  gMod = this;
+  
+  mPFN_makeKey_trampoline = patch::hookFunction(ttyd::system::makeKey, []()
+  {
+    gMod->updateEarly();
+  });
   
   // Initialize typesetting early
   ttyd::fontmgr::fontmgrTexSetup();
@@ -41,7 +41,7 @@ void Mod::updateEarly()
 {
   Mod::initClearStuff();
   Mod::setPitFloorsBeaten();
-	Mod::warpToBonetail();
+  Mod::warpToBonetail();
   Mod::overwritePipeBeroMap();
   Mod::overwriteBonetailChestItem();
   Mod::clearBonetailGSWFs();
@@ -53,9 +53,9 @@ void Mod::updateEarly()
     // --Tricks (With Display)--
     reinterpret_cast<Mod *>(user)->displayRoundNumber();
   }, this);
-	
-	// Call original function
-	mPFN_makeKey_trampoline();
+  
+  // Call original function
+  mPFN_makeKey_trampoline();
 }
 
 }
